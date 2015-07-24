2b2ccda7fc990254a6def839c878fa108fccbc5a
lxtlouise <xiaotingli@Xiaotings-MacBook-Pro.local>
2015-07-24 15:46:53 -0400

    test comment not finished
    test comment not finished

---
+++ src/infsci2955_deliverable5/TestComment.java
@@ -0,0 +1,34 @@
+package infsci2955_deliverable5;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class TestComment {
+	@Test
+	public void testComment(){
+		String email = "2014shufehey@gmail.com";
+		String pass = "2014shufe";
+		WebDriver driver = new FirefoxDriver();
+		  
+		driver.get("https://www.facebook.com/");
+
+		WebElement username = driver.findElement(By.id("email"));
+		username.sendKeys(email);
+		username.submit();
+		
+		WebElement password = driver.findElement(By.id("pass"));
+		password.sendKeys(pass);
+		password.submit();
+		
+		driver.get("https://www.facebook.com/");
+		WebElement comment = driver.findElement(By.cssSelector("a.comment_link > span"));
+		comment.sendKeys("good");
+
+		
+	}
+}
