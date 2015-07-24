1f69e9dd92630d4bf0270ee3186b8d6e76ba1f5c
msantraa <malini.santra@gmail.com>
2015-07-24 10:50:51 -0400

    search friends

--- src/infsci2955_deliverable5/TestSearch.java
+++ src/infsci2955_deliverable5/TestSearch.java
@@ -1,7 +1,6 @@
 package infsci2955_deliverable5;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -29,8 +28,15 @@ public class TestSearch {
 		WebElement password = driver.findElement(By.id("pass"));
 		password.sendKeys(pass);
 		password.submit();
+		
+		WebElement search=driver.findElement(By.className("_586i"));
+		search.sendKeys("Jie Chen");
+		driver.findElement(By.className("_5b1w")).click();
+	    driver.findElement(By.className("clearfix"));
+	    String strs=driver.findElement(By.className("clearfix")).getText();
+		assertTrue(strs.contains("jie chen"));
 		  
-		((JavascriptExecutor) driver).executeScript("document.getElementsByClassName('inputtext _586f hidden_elem').item(0).value = 'jie chen pittsburgh';");
+		//((JavascriptExecutor) driver).executeScript("document.getElementsByClassName('inputtext _586f hidden_elem').item(0).value = 'jie chen pittsburgh';");
 //        WebElement s = driver.findElement(By.tagName("button"));
 //        s.click();
 //		try {
