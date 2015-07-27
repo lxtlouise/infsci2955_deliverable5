939f7d65274236f4d15734344416cbdfb8d9c247
UsernameIsAlreadyTakenAgain <winnychen0828@gmail.com>
2015-07-26 22:07:43 -0400

    like page

---
+++ src/infsci2955_deliverable5/TestLikePage.java
@@ -0,0 +1,44 @@
+package infsci2955_deliverable5;
+
+import static org.junit.Assert.*;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class TestLikePage {
+	
+	static WebDriver driver=new FirefoxDriver(); 
+
+	@Before
+	public void setup() {
+		String email = "2014shufehey@gmail.com";
+		String pass = "2014shufe";  
+		driver.get("https://www.facebook.com/");
+	    
+		WebElement username = driver.findElement(By.id("email"));
+		username.sendKeys(email);
+		username.submit();
+		
+		WebElement password = driver.findElement(By.id("pass"));
+		password.sendKeys(pass);
+		password.submit();
+	}
+	
+	@Test
+	public void testLikePage(){		
+		driver.get("https://www.facebook.com/AdoreABullResuceCincinnati");
+		WebElement like=driver.findElement(By.id("u_0_2l"));
+		like.click();
+		String actual = like.getCssValue("color");
+		String expect="rgba(78, 86, 101, 1)";
+		System.out.println(actual);
+		assertEquals(expect,actual);
+	}
+
+}
