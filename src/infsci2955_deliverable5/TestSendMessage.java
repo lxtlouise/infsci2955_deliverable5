974d99a441e47228ebe4a3d57641bbac7873a58a
UsernameIsAlreadyTakenAgain <winnychen0828@gmail.com>
2015-07-26 20:05:25 -0400

    send message and share post

---
+++ src/infsci2955_deliverable5/TestSendMessage.java
@@ -0,0 +1,52 @@
+package infsci2955_deliverable5;
+
+import static org.junit.Assert.*;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class TestSendMessage {
+	
+	static WebDriver driver=new FirefoxDriver(); 
+
+	@Before
+	public void setup() {
+		String email = "2014shufehey@gmail.com";
+		String pass = "2014shufe";  
+		driver.get("https://www.facebook.com/");
+	    
+		WebElement username = driver.findElement(By.id("email"));
+		username.sendKeys(email);
+		username.submit();
+		
+		WebElement password = driver.findElement(By.id("pass"));
+		password.sendKeys(pass);
+		password.submit();
+	}
+	
+	//precondition: this message has never been send before
+	@Test
+	public void testSendMessage(){
+		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+		driver.findElement(By.id("fbDockChatBuddylistNub")).findElement(By.className("fbNubButton")).click();
+		WebElement friend=driver.findElement(By.id("u_0_25"));
+		friend.sendKeys("Jie");
+		friend.sendKeys(Keys.ENTER);
+		WebElement text=driver.findElement(By.cssSelector(".uiTextareaAutogrow._552m"));
+		String t="One";
+		text.sendKeys(t);
+		text.sendKeys(Keys.ENTER);
+		String content=driver.findElement(By.className("conversation")).getText();
+		System.out.println(content);
+		assertTrue(content.contains(t));
+		
+	}
+
+}
---
+++ src/infsci2955_deliverable5/TestSharePost.java
@@ -0,0 +1,62 @@
+package infsci2955_deliverable5;
+
+import static org.junit.Assert.*;
+
+import java.util.Iterator;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class TestSharePost {
+
+	static WebDriver driver=new FirefoxDriver(); 
+	
+	@Before
+	public void setup() {
+		String email = "2014shufehey@gmail.com";
+		String pass = "2014shufe";  
+		driver.get("https://www.facebook.com/");
+	    
+		WebElement username = driver.findElement(By.id("email"));
+		username.sendKeys(email);
+		username.submit();
+		
+		WebElement password = driver.findElement(By.id("pass"));
+		password.sendKeys(pass);
+		password.submit();
+	}
+	
+	//please add 1 to the total number of shares every time after running it
+	@Test
+	public void testSharePost(){
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.findElement(By.className("_2dpb")).click();
+		driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+		WebElement share=driver.findElement(By.id("tl_unit_860758939920290495"));
+	    WebElement button=share.findElement(By.className("share_root")).findElement(By.tagName("a"));
+	    JavascriptExecutor executor = (JavascriptExecutor)driver;
+	    executor.executeScript("arguments[0].click();", button);
+		driver.findElement(By.className("_54nh")).click();
+		//just wait some time
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.findElement(By.id("u_jsonp_2_n")).sendKeys(Keys.ENTER);
+		driver.navigate().refresh();
+		driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+		String s=driver.findElement(By.className("UFIShareLink")).getText();
+		//String s=driver.findElement(By.className("UFIShareLink")).getText();
+		System.out.println(s);
+		assertEquals(s,"2 shares");  //add one here
+	}
+
+}
